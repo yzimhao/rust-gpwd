5f3fbf8c110860f2422e042c5090f60d9c528365
dong <dong@q.com>
2018-02-28 10:52:04 +0800

    a

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+extern crate md5;
+extern crate rand;
+extern crate easy_strings;
+
+use std::env;
+use std::ops::{Range};
+use easy_strings::{EZString, ez};
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // string to int
+    // let maxlen = args[1].parse::<i32>().unwrap();
+    let maxlen: usize = args[1].parse().unwrap();
+
+
+    for i in 0..10{
+        let tuple = rand::random::<(f64, char)>();
+        let origin = tuple.0.to_string();
+        let digest = md5::compute(origin);
+
+        let m1 = format!("{:x}", digest);
+        let m2 = md5::compute(m1);
+        let m3 = format!("{:x}", m2);
+        let mut result = EZString::from(m3);
+
+        println!("{:02}: {}", i+1, result.substr(Range{start:0, end: maxlen}).to_string());
+        // println!("{:02}: {}", i+1, result.substr(0..maxlen).to_string());
+    }
+}
